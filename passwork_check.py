9961c06eb2181b7ce2a87c1ffbf1a6b24c701726
phoenyx <ph.03.nyx.c@gmail.com>
2020-04-03 16:36:55 +0800

    add password checker

---
+++ passwork_check.py
@@ -0,0 +1,6 @@
+password = input("Enter password of 8 characters minimum: ")
+while len(password) < 8:
+    print("Password must have at least 8 characters inclusive")
+    password = input("Enter password of 8 characters minimum: ")
+for a in password:
+    print("*", end="")
